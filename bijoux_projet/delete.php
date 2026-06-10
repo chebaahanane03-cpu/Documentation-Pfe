b22415435ba1c0617ac1b0804094f81565d63837
chebaahanane03-cpu <chebaahanane03@gmail.com>
2026-06-10 13:16:17 +0100

    Codage de la solution

---
+++ bijoux_projet/delete.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'db.php';
+require 'functions.php';
+
+if (!isset($_GET['id'])) {
+header("Location:index.php");
+exit;
+}
+
+$id=$_GET['id'];
+
+deleteProduct($pdo,$id);
+
+header("Location:index.php");
+exit;
+
+?>
